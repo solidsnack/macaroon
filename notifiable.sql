8edc174890b035eb3e4c3c1b2c4261713fb4dd8e
Jason Dusek <jason.dusek@gmail.com>
2015-07-19 20:31:29 -0700

    Update so it's actually working.

--- notifiable.sql
+++ notifiable.sql
@@ -8,25 +8,27 @@ SET LOCAL search_path TO notifiable, public;
 CREATE OR REPLACE FUNCTION setup(tab regclass, channel text)
 RETURNS void AS $$
 BEGIN
-  EXECUTE codegen(tab, channel);
+  EXECUTE notifiable.codegen(tab, channel);
 END
 $$ LANGUAGE plpgsql;
 COMMENT ON FUNCTION setup(regclass, text) IS
  'Sets up RULEs such that every INSERT, UPDATE and DELETE to a table will '
  'result in a notification on the chosen channel. The notification payload is '
- 'one of `+`, `~` or `-` depending on whether the action is an INSERT, UPDATE '
- 'or DELETE.';
+ 'the transaction ID (to allow correlation with audit logs).';
 
 CREATE OR REPLACE FUNCTION codegen(tab regclass, channel text)
 RETURNS text AS $code$
 BEGIN
   RETURN $$
-    CREATE OR REPLACE RULE notifiable_ins AS ON INSERT TO $$||tab||$$
-        DO ALSO NOTIFY $$||quote_ident(channel)||$$, '+';
-    CREATE OR REPLACE RULE notifiable_upd AS ON UPDATE TO $$||tab||$$
-        DO ALSO NOTIFY $$||quote_ident(channel)||$$, '~';
-    CREATE OR REPLACE RULE notifiable_del AS ON DELETE TO $$||tab||$$
-        DO ALSO NOTIFY $$||quote_ident(channel)||$$, '-';
+    CREATE OR REPLACE RULE notify_ins AS ON INSERT TO $$||tab||$$
+        DO ALSO SELECT pg_notify($$||quote_literal(channel)||$$,
+                                 txid_current()::text);
+    CREATE OR REPLACE RULE notify_upd AS ON UPDATE TO $$||tab||$$
+        DO ALSO SELECT pg_notify($$||quote_literal(channel)||$$,
+                                 txid_current()::text);
+    CREATE OR REPLACE RULE notify_del AS ON DELETE TO $$||tab||$$
+        DO ALSO SELECT pg_notify($$||quote_literal(channel)||$$,
+                                 txid_current()::text);
   $$;
 END
 $code$ LANGUAGE plpgsql;
